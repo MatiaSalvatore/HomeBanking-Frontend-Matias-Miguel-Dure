ae14e99c6d9229b7919076f6c06dd069b8fd892d
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-02-21 11:47:02 -0300

    Forms fixed

--- src/pages/ReqLoan.jsx
+++ src/pages/ReqLoan.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Anchor from "../components/Anchor";
 
 const ReqLoan = () => {
-
     /* 
        post
          axios.post("http://localhost:8080/api/account")
@@ -17,19 +16,23 @@ const ReqLoan = () => {
  
  */
 
+
    return (
     <>
     <div className="componentcontainer">
         <h3>Add a new loan</h3>
         <form action="" method="post" className="Form">
           <fieldset className="Formfield">
-            <label>Dato: <input type="text" id="1" /></label>
-            <label>Dato: <input type="text" id="2" /></label>
-            <label>Dato: <input type="text" id="3" /></label>
-            <label>Dato: <input type="text" id="4" /></label>
-            <label>Dato: <input type="text" id="5" /></label>
-            <label>Dato: <input type="text" id="6" /></label>
-            <label>Dato: <input type="text" id="7" /></label>
+            <label>Reason for Loan: <input type="text" id="1" /></label>
+            <label>Account of origin: <input type="text" id="2" /></label>
+            <label>Amount: $<input type="number" id="3" /></label>
+            <label>Payments: <select className="selectCard" name="ej" id="ej">
+              <option value="pay1">3</option>
+              <option value="pay2">6</option>
+              <option value="pay3">12</option>
+              <option value="pay4">24</option>
+            </select></label>
+            <Anchor href="/loans" content="Submit"></Anchor>
           </fieldset>
         </form>
     </div>
